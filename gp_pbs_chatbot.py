a104a8567be18b71d778b195f52fb5176bde2c9a
Ben Batman <benbatman2@gmail.com>
2023-10-24 10:22:37 -0400

    Update gp_pbs_chatbot.py
    Added streamlit secrets for api keys

--- gp_pbs_chatbot.py
+++ gp_pbs_chatbot.py
@@ -1,80 +1,26 @@
-import streamlit as st 
-import json
-from sentence_transformers import SentenceTransformer
-from langchain.embeddings.openai import OpenAIEmbeddings 
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.chat_models import ChatOpenAI
-from langchain.vectorstores import FAISS
-from langchain.chains.query_constructor.base import AttributeInfo
-from langchain.schema import Document
-from langchain.chains import ConversationalRetrievalChain
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.memory import ConversationBufferMemory
+from tools import create_and_store_embeddings, ask_and_get_answer, pretty_print_docs
+from chat_streaming import StreamHandler
 
 
 #################################################################
 
-# TODO: put all helper functions into tools.py file 
-
-def create_and_store_embeddings():
-    #embeddings=OpenAIEmbeddings() # if you're using openAI
-    model_name = 'sentence-transformers/all-mpnet-base-v2'
-    embeddings = HuggingFaceEmbeddings(model_name=model_name)
-    #embeddings = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
-    vectorstore = FAISS.load_local("faiss_index", embeddings)
-    return vectorstore
-
-def get_memory():
-    """
-    Instantiate memory for LLM chain. 
-    """
-    memory = ConversationBufferMemory(memory_key='history', return_messages=True)
-
-def ask_and_get_answer(vectorstore, q, chat_history, k=5):
-   
-    llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0.5)
-
-    docs = vectorstore.similarity_search(q) # Find relevant documents 
-
-    context = "" 
-
-    doc_sources_string = ""
-    for doc in docs:
-        #doc_sources_string += docs.metadata['sources'] + "\n"
-        context += doc.page_content # combine all relevant docs into one variable 
-
-    # Instantiate the conversation chain (can take in chat history as arg to maintain memory)
-    chain = ConversationalRetrievalChain.from_llm(
-            llm=llm,
-            retriever=vectorstore.as_retriever(search_type="similarity"),
-            #memory=get_memory(),
-            return_source_documents = True
-            )
-    
-    # Define the system prompt
-    PROMPT = """You are a helpful assistant that has access to lots of documents from PBS. 
-    You this context to answer the question at the end. Give a detailed, long answer.
-    Recommend URLs where applicable"""
-
-    query = PROMPT + "\n" + f"{context}" + "\n" + q # Combine all aspects of the query
-
-    answer = chain({'question':query, "chat_history":chat_history}) # run the chain, takes in the query and the chat history
-
-    return answer, docs
-
-def print_embedding_cost(texts):
-  import tiktoken
-  enc = tiktoken.encoding_for_model ('text-embedding-ada-002')
-  total_tokens = sum([len(enc.encode(page.page_content)) for page in texts])
-  return total_tokens, total_tokens / 1000 * 0.0004
-
-
+# TODO: 
+# Figure out why second query malfunctions sometimes and doesn't return correct information
+# Prompt user to clarify if Ai can't find enough relevant information --- Done
+# if user question is too broad prompt to be more specific 
+# Figure out how not to stream multiqueries if using the multiquery library
+# Should a confidence level be added to the answers?
 
 def main():
     import os 
-    # from dotenv import load_dotenv, find_dotenv
-    # load_dotenv(find_dotenv(), override=True)
-    os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY'] # Specific to cloud deployment with streamlit
+    #from dotenv import load_dotenv, find_dotenv
+    import streamlit as st
+    #load_dotenv(find_dotenv(), override=True) # loading openai api key and cohere api key as environment variables
+    os.environ['TOKENIZERS_PARALLELISM'] = 'false'
+    os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+    os.environ['PINECONE_API_KEY'] = st.secrets['PINECONE_API_KEY']
+    os.environ['PINECONE_ENV'] = st.secrets['PINECONE_ENV']
+    os.environ['COHERE_API_KEY'] = st.secrets['COHERE_API_KEY']
 
 
     st.set_page_config(
@@ -85,50 +31,77 @@ def main():
     st.image('PBS_logo.png')
     st.subheader('Your PBS Chatbot powered by GenAI')
 
-     
-    # Create the chunks and vectorestore
-    with st.spinner('Your PBS chatbot is getting ready'):
-        vectorstore = create_and_store_embeddings()
-        st.session_state.vs = vectorstore
-        
-        # tokens, embedding_cost = print_embedding_cost(chunks)
-        # st.write(f"Embedding cost: ${embedding_cost:.4f}")
-        # st.write(f"Total tokens: {tokens}")
+    with st.sidebar:
+        temperature = float(st.number_input("Creativity level: 0 -> Not creative | 2 -> Very creative", value=0.01, min_value=0.01, max_value=2.0, step=0.1))
 
-    # Save messages to the session state 
+        filter_value = st.selectbox("Want to filter the data?", ('title', 'language'), index=None)
+
+    # Create the chunks and vectorestore
+    if 'vs' not in st.session_state:
+        with st.sidebar:
+            with st.spinner('Your PBS chatbot is getting ready'):
+                vectorstore = create_and_store_embeddings()
+                st.session_state.vs = vectorstore
+                
+                # tokens, embedding_cost = print_embedding_cost(chunks)
+                # st.write(f"Embedding cost: ${embedding_cost:.4f}")
+                # st.write(f"Total tokens: {tokens}")
+
+    # Save messages to the session state if not already
     if 'messages' not in st.session_state:
         st.session_state.messages = []
     
-    if 'memory' not in st.session_state:
-        st.session_state.memory = get_memory()
+    # if 'memory' not in st.session_state:
+    #     st.session_state.memory = get_memory()
 
     chat_history = st.session_state.messages
 
-    q = st.text_input('Ask a question')
+    question = st.text_input('Chat with your custom PBS assistant!')
+    
+    if question:
+        if 'vs' in st.session_state:
+            vectorstore = st.session_state.vs
+            chat_box=st.empty()
+            stream_handler = StreamHandler(chat_box)
+            response, rel_docs, urls, no_context_answer = ask_and_get_answer(vectorstore, question, chat_history=chat_history, temperature=temperature, stream_handler=stream_handler)
+            # st.text_area('LLM Answer:', value=response['answer'], height=400)
+            print(response)
+            #st.write(response['answer'])
+            # st.markdown(response['answer'])
 
-    with st.spinner('Working on your request'):
-        if q:
-            if 'vs' in st.session_state:
-                vectorstore = st.session_state.vs
-                response, docs = ask_and_get_answer(vectorstore, q, chat_history=chat_history)
-                st.text_area('LLM Answer:', value=response['answer'])
-                st.divider()
-                st.text_area('Source documents', value=docs)
-                chat_history.append((q, response['answer']))
-                st.session_state.messages = chat_history
+            st.subheader("Chat history", divider='blue')
+            if 'history' not in st.session_state:
+                st.session_state.history = ''
+
+            value = f'Question: {question} \nAnswer: {response["answer"]}'
+            st.session_state.history = f'{value} \n {"*"*100} \n {st.session_state.history}'
+            h = st.session_state.history
+            # st.text_area(label='Chat History', value=h, key='hsistory', height=400)
+            st.write(h)
+
+            
+            for doc in rel_docs:
+                if not doc.metadata['relevance_score'] < 0.5:
+                    st.subheader("Sources", divider='blue')
+                    st.write(urls)
+
+                    # Relevant documents retrieved by similarity search, comment out to remove from printing onto streamlit 
+                    st.subheader("Documents sourced by AI", divider="blue")
+                    rel_docs = pretty_print_docs(rel_docs)
+                    st.write(rel_docs)
+                    #st.text_area("Docs", value=rel_docs)
+
+            chat_history.append((question, response['answer']))
+            st.session_state.messages = chat_history
 
 
-                st.divider()
-                if 'history' not in st.session_state:
-                    st.session_state.history = ''
-
-                value = f'q: {q} \nA: {response["answer"]}'
-                st.session_state.history = f'{value} \n {"*"*100} \n {st.session_state.history}'
-                h = st.session_state.history
-                st.text_area(label='Chat History', value=h, key='history', height=400)
+    # for i, msg in enumerate(st.session_state.messages[1:]):
+    #     if i % 2 == 0:
+    #         message(msg.content, is_user=True)
+    #     else:
+    #         message(msg.content, is_user=False)
 
 ###############################################
-
 
 if __name__ == "__main__":
    main()
